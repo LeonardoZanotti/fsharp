41e0771ce9b862b89678d5520a3d7c644aa87f5a
Leonardo <leonardojosezanotti@gmail.com>
2020-03-05 12:06:51 -0300

    F# files

---
+++ Contador de números/ConsoleApplication1/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+let myList = [0..9]
+let myFunction =
+for n in myList do
+    Console.WriteLine(n)
+
+Console.ReadKey()
---
+++ Hello World/ConsoleApplication1/Program.fs
@@ -0,0 +1,7 @@
+﻿open System // import Namespace         // Aparentemente é um modo de se iniciar as linhas de código e permitir o uso dos comandos Console
+
+Console.WriteLine("Hello, World!")      // Um jeito de escrever 
+
+Console.WriteLine("")                   
+printfn "Hello, World!"                 // Jeito principal de escrever
+Console.ReadKey()                       // Abre o cmd que vai ler o código e mostrá-lo, encerrando o processo
